36f63a924ac6d61b6d66d1adaadb83f919bc2546
FuturelessFestival <106823095+FuturelessFestival@users.noreply.github.com>
2022-07-18 01:42:52 +0200

    Update javascript2.js

--- javascript2.js
+++ javascript2.js
@@ -11,52 +11,11 @@ var intervalId = window.setInterval(function(){
 
 
 function myFunction() {
-  console.log(op);
   var elem = document.getElementById('fadeInOut');
-  test = fade(elem);
-  console.log(op);
   if(point < texts.length - 1){
     point ++;
   }else{
     point = 0;
   }
   elem.innerHTML = texts[point];
-  test = unfade(elem);
-}
-
-function fade(element) {
-    while(op > 0){
-      element.style.opacity = op;
-      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-      op = op - 0.01;
-    }
-  
-  //var op = 1;  // initial opacity
-    //var timer = setInterval(function () {
-    //    if (op <= 0.1){
-    //        clearInterval(timer);
-    //        element.style.opacity = 0;
-    //    }
-    //    element.style.opacity = op;
-    //    element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-    //    op -= op * 0.1;
-    //}, 50);
-}
-function unfade(element) {
-    while(op < 0){
-      element.style.opacity = op;
-      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-      op = op + 0.01;
-    }
-  
-  //var op = 0.1;  // initial opacity
-  // element.style.display = 'block';
-  //  var timer = setInterval(function () {
-  //      if (op >= 1){
-  //          clearInterval(timer);
-  //      }
-  //      element.style.opacity = op;
-  //      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-  //      op += op * 0.1;
-  //  }, 50);
 }
