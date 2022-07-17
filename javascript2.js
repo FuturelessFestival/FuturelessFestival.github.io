1e9d15720e845eb9feaeb04b994845fc245642bc
FuturelessFestival <106823095+FuturelessFestival@users.noreply.github.com>
2022-07-18 01:26:09 +0200

    Update javascript2.js

--- javascript2.js
+++ javascript2.js
@@ -24,26 +24,38 @@ function myFunction() {
 }
 
 function fade(element) {
-    //var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1){
-            clearInterval(timer);
-            element.style.opacity = 0;
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-    }, 50);
+    while(op > 0){
+      element.style.opacity = op;
+      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+      op = op - 0.01;
+    }
+  
+  //var op = 1;  // initial opacity
+    //var timer = setInterval(function () {
+    //    if (op <= 0.1){
+    //        clearInterval(timer);
+    //        element.style.opacity = 0;
+    //    }
+    //    element.style.opacity = op;
+    //    element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+    //    op -= op * 0.1;
+    //}, 50);
 }
 function unfade(element) {
-    //var op = 0.1;  // initial opacity
-    element.style.display = 'block';
-    var timer = setInterval(function () {
-        if (op >= 1){
-            clearInterval(timer);
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1;
-    }, 50);
+    while(op < 0){
+      element.style.opacity = op;
+      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+      op = op + 0.01;
+    }
+  
+  //var op = 0.1;  // initial opacity
+  // element.style.display = 'block';
+  //  var timer = setInterval(function () {
+  //      if (op >= 1){
+  //          clearInterval(timer);
+  //      }
+  //      element.style.opacity = op;
+  //      element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+  //      op += op * 0.1;
+  //  }, 50);
 }
