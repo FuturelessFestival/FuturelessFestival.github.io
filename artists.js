ff4cbdaef44a6a18e687f363e3a0aae8f7ba963c
FuturelessFestival <106823095+FuturelessFestival@users.noreply.github.com>
2022-08-03 16:40:50 +0200

    Add files via upload

---
+++ artists.js
@@ -0,0 +1,255 @@
+function artist1() {
+  var moreText = document.getElementById("artistText1");
+  var btn = document.getElementById("artistBtn1");
+  var dots = document.getElementById("artistAbout1")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist2() {
+  var moreText = document.getElementById("artistText2");
+  var btn = document.getElementById("artistBtn2");
+  var dots = document.getElementById("artistAbout2")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist3() {
+  var moreText = document.getElementById("artistText3");
+  var btn = document.getElementById("artistBtn3");
+  var dots = document.getElementById("artistAbout3")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist4() {
+  var moreText = document.getElementById("artistText4");
+  var btn = document.getElementById("artistBtn4");
+  var dots = document.getElementById("artistAbout4")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist5() {
+  var moreText = document.getElementById("artistText5");
+  var btn = document.getElementById("artistBtn5");
+  var dots = document.getElementById("artistAbout5")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist6() {
+  var moreText = document.getElementById("artistText6");
+  var btn = document.getElementById("artistBtn6");
+  var dots = document.getElementById("artistAbout6")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist7() {
+  var moreText = document.getElementById("artistText7");
+  var btn = document.getElementById("artistBtn7");
+  var dots = document.getElementById("artistAbout7")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist8() {
+  var moreText = document.getElementById("artistText8");
+  var btn = document.getElementById("artistBtn8");
+  var dots = document.getElementById("artistAbout8")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist9() {
+  var moreText = document.getElementById("artistText9");
+  var btn = document.getElementById("artistBtn9");
+  var dots = document.getElementById("artistAbout9")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist10() {
+  var moreText = document.getElementById("artistText10");
+  var btn = document.getElementById("artistBtn10");
+  var dots = document.getElementById("artistAbout10")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist11() {
+  var moreText = document.getElementById("artistText11");
+  var btn = document.getElementById("artistBtn11");
+  var dots = document.getElementById("artistAbout11")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist12() {
+  var moreText = document.getElementById("artistText12");
+  var btn = document.getElementById("artistBtn12");
+  var dots = document.getElementById("artistAbout12")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist13() {
+  var moreText = document.getElementById("artistText13");
+  var btn = document.getElementById("artistBtn13");
+  var dots = document.getElementById("artistAbout13")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist14() {
+  var moreText = document.getElementById("artistText14");
+  var btn = document.getElementById("artistBtn14");
+  var dots = document.getElementById("artistAbout14")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist15() {
+  var moreText = document.getElementById("artistText15");
+  var btn = document.getElementById("artistBtn15");
+  var dots = document.getElementById("artistAbout15")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist16() {
+  var moreText = document.getElementById("artistText16");
+  var btn = document.getElementById("artistBtn16");
+  var dots = document.getElementById("artistAbout16")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
+function artist17() {
+  var moreText = document.getElementById("artistText17");
+  var btn = document.getElementById("artistBtn17");
+  var dots = document.getElementById("artistAbout17")
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline-block";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline-block";
+  }
+}
