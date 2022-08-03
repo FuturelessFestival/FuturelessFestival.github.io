a143235b5de76bb33963f4124c6f1e5f93b52887
FuturelessFestival <106823095+FuturelessFestival@users.noreply.github.com>
2022-08-03 19:44:13 +0200

    Update artists.js

--- artists.js
+++ artists.js
@@ -1,7 +1,7 @@
 function artist1() {
   var moreText = document.getElementById("artistText1");
   var btn = document.getElementById("artistBtn1");
-  var dots = document.getElementById("artistAbout1")
+  var dots = document.getElementById("artistAbout1");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -16,7 +16,7 @@ function artist1() {
 function artist2() {
   var moreText = document.getElementById("artistText2");
   var btn = document.getElementById("artistBtn2");
-  var dots = document.getElementById("artistAbout2")
+  var dots = document.getElementById("artistAbout2");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -31,22 +31,25 @@ function artist2() {
 function artist3() {
   var moreText = document.getElementById("artistText3");
   var btn = document.getElementById("artistBtn3");
-  var dots = document.getElementById("artistAbout3")
+  var dots = document.getElementById("artistAbout3");
+  var link = document.getElementById("artistLink3");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
     btn.innerHTML = "Read more";
     moreText.style.display = "none";
+    link.style.display = "none";
   } else {
     dots.style.display = "none";
     btn.innerHTML = "Read less";
     moreText.style.display = "inline-block";
+    link.style.display = "inline-block";
   }
 }
 function artist4() {
   var moreText = document.getElementById("artistText4");
   var btn = document.getElementById("artistBtn4");
-  var dots = document.getElementById("artistAbout4")
+  var dots = document.getElementById("artistAbout4");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -61,7 +64,7 @@ function artist4() {
 function artist5() {
   var moreText = document.getElementById("artistText5");
   var btn = document.getElementById("artistBtn5");
-  var dots = document.getElementById("artistAbout5")
+  var dots = document.getElementById("artistAbout5");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -76,7 +79,7 @@ function artist5() {
 function artist6() {
   var moreText = document.getElementById("artistText6");
   var btn = document.getElementById("artistBtn6");
-  var dots = document.getElementById("artistAbout6")
+  var dots = document.getElementById("artistAbout6");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -91,7 +94,7 @@ function artist6() {
 function artist7() {
   var moreText = document.getElementById("artistText7");
   var btn = document.getElementById("artistBtn7");
-  var dots = document.getElementById("artistAbout7")
+  var dots = document.getElementById("artistAbout7");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -106,7 +109,7 @@ function artist7() {
 function artist8() {
   var moreText = document.getElementById("artistText8");
   var btn = document.getElementById("artistBtn8");
-  var dots = document.getElementById("artistAbout8")
+  var dots = document.getElementById("artistAbout8");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -121,7 +124,7 @@ function artist8() {
 function artist9() {
   var moreText = document.getElementById("artistText9");
   var btn = document.getElementById("artistBtn9");
-  var dots = document.getElementById("artistAbout9")
+  var dots = document.getElementById("artistAbout9");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -136,7 +139,7 @@ function artist9() {
 function artist10() {
   var moreText = document.getElementById("artistText10");
   var btn = document.getElementById("artistBtn10");
-  var dots = document.getElementById("artistAbout10")
+  var dots = document.getElementById("artistAbout10");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -151,7 +154,7 @@ function artist10() {
 function artist11() {
   var moreText = document.getElementById("artistText11");
   var btn = document.getElementById("artistBtn11");
-  var dots = document.getElementById("artistAbout11")
+  var dots = document.getElementById("artistAbout11");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -166,7 +169,7 @@ function artist11() {
 function artist12() {
   var moreText = document.getElementById("artistText12");
   var btn = document.getElementById("artistBtn12");
-  var dots = document.getElementById("artistAbout12")
+  var dots = document.getElementById("artistAbout12");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -181,7 +184,7 @@ function artist12() {
 function artist13() {
   var moreText = document.getElementById("artistText13");
   var btn = document.getElementById("artistBtn13");
-  var dots = document.getElementById("artistAbout13")
+  var dots = document.getElementById("artistAbout13");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -196,7 +199,7 @@ function artist13() {
 function artist14() {
   var moreText = document.getElementById("artistText14");
   var btn = document.getElementById("artistBtn14");
-  var dots = document.getElementById("artistAbout14")
+  var dots = document.getElementById("artistAbout14");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -211,7 +214,7 @@ function artist14() {
 function artist15() {
   var moreText = document.getElementById("artistText15");
   var btn = document.getElementById("artistBtn15");
-  var dots = document.getElementById("artistAbout15")
+  var dots = document.getElementById("artistAbout15");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -226,7 +229,7 @@ function artist15() {
 function artist16() {
   var moreText = document.getElementById("artistText16");
   var btn = document.getElementById("artistBtn16");
-  var dots = document.getElementById("artistAbout16")
+  var dots = document.getElementById("artistAbout16");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
@@ -241,7 +244,7 @@ function artist16() {
 function artist17() {
   var moreText = document.getElementById("artistText17");
   var btn = document.getElementById("artistBtn17");
-  var dots = document.getElementById("artistAbout17")
+  var dots = document.getElementById("artistAbout17");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline-block";
